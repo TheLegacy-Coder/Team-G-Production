479d5a6a182c1b4a54529b167e91082c3bfaeabf
Dimitri Saliba <dimitriasaliba@gmail.com>
2024-01-23 17:23:58 -0500

    Clean code and add text box

--- apps/frontend/src/components/InteractableMap.tsx
+++ apps/frontend/src/components/InteractableMap.tsx
@@ -15,10 +15,11 @@ export const InteractableMap = () => {
   const ctx = canvasCtxRef.current;
 
   const image = new Image();
+  image.src = "00_thelowerlevel1.png";
   setTimeout(forceUpdate, 100);
-  image.onload = () => {
-    if (ctx == null) return;
 
+  function draw() {
+    if (ctx == null) return;
     ctx?.drawImage(image, 0, 0);
     let last: MapNode | undefined = undefined;
     ctx!.strokeStyle = "#0000FF";
@@ -41,20 +42,26 @@ export const InteractableMap = () => {
       ctx!.strokeStyle = "#330000";
       ctx!.stroke();
       if (node == hl || node == sl) {
+        ctx!.fillStyle = "#FFFFFF";
+        ctx!.strokeStyle = "#000000";
+        ctx!.fillRect(node.xcoord - 80, node.ycoord + 15, 160, 20);
+        ctx!.strokeRect(node.xcoord - 80, node.ycoord + 15, 160, 20);
         ctx!.font = "bold 10pt Courier";
         ctx!.textAlign = "center";
         ctx!.fillStyle = "#550000";
-        ctx!.fillText(node.shortName, node.xcoord, node.ycoord + 25);
+        ctx!.fillText(node.shortName, node.xcoord, node.ycoord + 28);
       }
     });
-  };
+  }
 
-  image.src = "00_thelowerlevel1.png";
+  image.onload = () => {
+    draw();
+  };
 
   function mouseUp(evt: React.MouseEvent<Element, MouseEvent>) {
     const x = evt.pageX - xOffset;
     const y = evt.pageY - yOffset;
-    ctx?.drawImage(image, 0, 0);
+
     let emptyClick = true;
     mapNodes.forEach((node) => {
       const dist = Math.sqrt(
@@ -75,26 +82,13 @@ export const InteractableMap = () => {
       sl = undefined;
       path = [];
     }
-    mouseMove(evt);
+    draw();
   }
   function mouseMove(evt: React.MouseEvent<Element, MouseEvent>) {
     if (ctx == null) return;
 
     const x = evt.pageX - xOffset;
     const y = evt.pageY - yOffset;
-    ctx?.drawImage(image, 0, 0);
-
-    let last: MapNode | undefined = undefined;
-    ctx!.strokeStyle = "#0000FF";
-    path.forEach((p) => {
-      if (last != undefined) {
-        ctx!.beginPath(); // Start a new path
-        ctx!.moveTo(last.xcoord, last.ycoord); // Move the pen to (30, 50)
-        ctx!.lineTo(p.xcoord, p.ycoord); // Draw a line to (150, 100)
-        ctx!.stroke(); // Render the path
-      }
-      last = p;
-    });
 
     mapNodes.forEach((node) => {
       const dist = Math.sqrt(
@@ -103,38 +97,11 @@ export const InteractableMap = () => {
       if (dist < 10 && path.length == 0) {
         hl = node;
         forceUpdate();
-        ctx!.beginPath();
-        ctx!.arc(node.xcoord, node.ycoord, 10, 0, 2 * Math.PI, false);
-        ctx!.fillStyle =
-          sl == node ? "#00FF00" : hl == node ? "#0000FF" : "#FF0000";
-        ctx!.fill();
-        ctx!.lineWidth = 5;
-        ctx!.strokeStyle = "#330000";
-        ctx!.stroke();
-        if (node == hl || node == sl) {
-          ctx!.font = "bold 10pt Courier";
-          ctx!.textAlign = "center";
-          ctx!.fillStyle = "#550000";
-          ctx!.fillText(node.shortName, node.xcoord, node.ycoord + 25);
-        }
       } else {
         if (hl == node && path.length == 0) hl = undefined;
-        ctx!.beginPath();
-        ctx!.arc(node.xcoord, node.ycoord, 10, 0, 2 * Math.PI, false);
-        ctx!.fillStyle =
-          sl == node ? "#00FF00" : hl == node ? "#0000FF" : "#FF0000";
-        ctx!.fill();
-        ctx!.lineWidth = 5;
-        ctx!.strokeStyle = "#330000";
-        ctx!.stroke();
-        if (node == hl || node == sl) {
-          ctx!.font = "bold 10pt Courier";
-          ctx!.textAlign = "center";
-          ctx!.fillStyle = "#550000";
-          ctx!.fillText(node.shortName, node.xcoord, node.ycoord + 25);
-        }
       }
     });
+    draw();
   }
 
   useEffect(() => {
