deb1e539b2a1c74d4f6169f820ffcbce28f05e7c
TheLegacy-Coder <klaudiofushakako@gmail.com>
2024-01-22 20:01:49 -0500

    Moving login features to the actual login page. Clearing home page.

--- apps/frontend/src/components/ProtectedRoutes.tsx
+++ apps/frontend/src/components/ProtectedRoutes.tsx
@@ -4,7 +4,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 export const ProtectedRoutes = () => {
   // TODO: Use authentication token
-  const auth = false; //TODO: add auth check
+  const auth = true; //TODO: add auth check
 
   return auth ? <Outlet /> : <Navigate to="/login" replace />;
 };
