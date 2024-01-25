00d724b6adb42a300b3fd3110cb81f47cc97cb50
zmedailleu <zacharymedailleu@gmail.com>
2024-01-24 19:33:05 -0500

    unfinished attempt at making the line draw progressively

--- apps/frontend/src/components/InteractableMap.tsx
+++ apps/frontend/src/components/InteractableMap.tsx
@@ -8,6 +8,7 @@ let xOffset = 0;
 let hl: MapNode | undefined = undefined;
 let sl: MapNode | undefined = undefined;
 let path: MapNode[] = [];
+let flip = false;
 export const InteractableMap = () => {
   const [, forceUpdate] = useReducer((x) => x + 1, 0);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
@@ -18,42 +19,57 @@ export const InteractableMap = () => {
   image.src = "00_thelowerlevel1.png";
   setTimeout(forceUpdate, 100);
 
-  function delay(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-
   function draw() {
+    let drawStep = 0;
     if (ctx == null) return;
     ctx?.drawImage(image, 0, 0);
     let last: MapNode | undefined = undefined;
-    ctx!.strokeStyle = "#0000FF";
+    let currentLight = flip;
+
     path.forEach((p) => {
       if (last != undefined) {
-        let xstart: number = p.xcoord;
-        let ystart: number = p.ycoord;
+        drawStep++;
+        const distance: number = Math.sqrt(
+          Math.pow(p.xcoord - last.xcoord, 2) +
+            Math.pow(p.ycoord - last.ycoord, 2),
+        );
+        let xfinish: number = p.xcoord;
+        let yfinish: number = p.ycoord;
+        const guideSpacer: number = Math.round(distance / 25);
 
-        while (xstart != last.xcoord || ystart != last.ycoord) {
-          const xfinish: number = xstart;
-          const yfinish: number = ystart;
-          if (xstart < last.xcoord) {
-            xstart++;
-          } else if (xstart > last.xcoord) {
-            xstart--;
+        while (xfinish != last.xcoord || yfinish != last.ycoord) {
+          const xstart: number = xfinish;
+          const ystart: number = yfinish;
+          //animation goes one pixel to the next, check which direction to draw next pixel
+          if (xfinish < last.xcoord) {
+            xfinish++;
+          } else if (xfinish > last.xcoord) {
+            xfinish--;
           }
-          if (ystart < last.ycoord) {
-            ystart++;
-          } else if (ystart > last.ycoord) {
-            ystart--;
+          if (yfinish < last.ycoord) {
+            yfinish++;
+          } else if (yfinish > last.ycoord) {
+            yfinish--;
           }
-          setTimeout(delay, 1000);
+          if (drawStep % guideSpacer == 0) {
+            ctx!.beginPath();
+            ctx!.arc(xfinish, yfinish, 20, 0, 2 * Math.PI, false);
+            ctx!.fillStyle = "#FF80ED";
+            ctx!.fill();
+            ctx!.stroke();
+          }
+          ctx!.strokeStyle = currentLight ? "#0000FF" : "#00FF00"; //choose color of pixel
           ctx!.beginPath(); // Start a new path
-          ctx!.moveTo(xfinish, yfinish); // Move the pen to (30, 50)
-          ctx!.lineTo(xstart, ystart); // Draw a line to (150, 100)
+          ctx!.moveTo(xstart, ystart); // Move the pen to (30, 50)
+          ctx!.lineTo(xfinish, yfinish); // Draw a line to (150, 100)
           ctx!.stroke(); // Render the path
+
+          currentLight = !currentLight;
         }
       }
       last = p;
     });
+    flip = !flip;
     mapNodes.forEach((node) => {
       ctx!.beginPath();
       ctx!.arc(node.xcoord, node.ycoord, 10, 0, 2 * Math.PI, false);
@@ -74,10 +90,14 @@ export const InteractableMap = () => {
         ctx!.fillText(node.shortName, node.xcoord, node.ycoord + 28);
       }
     });
+
+    forceUpdate();
+    if (path.length > 0) setTimeout(draw, 100);
+    console.log("drawStep:", drawStep);
   }
 
   image.onload = () => {
-    draw();
+    //draw();
   };
 
   function mouseUp(evt: React.MouseEvent<Element, MouseEvent>) {
@@ -111,19 +131,22 @@ export const InteractableMap = () => {
 
     const x = evt.pageX - xOffset;
     const y = evt.pageY - yOffset;
-
+    let changed = false;
     mapNodes.forEach((node) => {
       const dist = Math.sqrt(
         Math.pow(x - node.xcoord, 2) + Math.pow(y - node.ycoord, 2),
       );
       if (dist < 10 && path.length == 0) {
         hl = node;
-        forceUpdate();
+        changed = true;
       } else {
-        if (hl == node && path.length == 0) hl = undefined;
+        if (hl == node && path.length == 0 && hl != undefined) {
+          changed = true;
+          hl = undefined;
+        }
       }
     });
-    draw();
+    if (changed) draw();
   }
 
   useEffect(() => {
