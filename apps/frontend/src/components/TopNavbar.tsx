df01e568e3bcfbdfbd634629f0835be628a459b6
Gabriel Shiu <gabriel@shiu.us>
2024-01-30 20:35:57 -0500

    added admin login and csv button for admins only

--- apps/frontend/src/components/TopNavbar.tsx
+++ apps/frontend/src/components/TopNavbar.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./styles/TopNavbar.css";
 import { Nav, Navbar, Container } from "react-bootstrap";
 import { CloseProgram } from "../components/CloseProgram.tsx";
 import { Link } from "react-router-dom";
 import { ContextMenuRouterButton } from "./ContextMenuRouterButton.tsx";
 import { Login } from "../routes/Login.tsx";
+import { loginStore } from "../stores/LoginStore.ts";
 
 export const TopNavbar = () => {
+  const AdminButtons = () => {
+    const [, forceUpdate] = useReducer((x) => x + 1, 0);
+    loginStore.navbarRefreshHook = forceUpdate;
+    console.log(loginStore.loginType, loginStore.loggedIn);
+    if (loginStore.loginType === "admin" && loginStore.loggedIn) {
+      return (
+        <div>
+          <Link to="/csvs" style={{ textDecoration: "none" }}>
+            CSVs
+          </Link>
+          <div>&nbsp;&nbsp;&nbsp;</div>
+        </div>
+      );
+    }
+    return <></>;
+  };
   return (
     <Navbar bg="light" data-bs-theme="light">
       <Container>
@@ -37,6 +54,10 @@ export const TopNavbar = () => {
           <div>&nbsp;&nbsp;&nbsp;</div>
 
           <ContextMenuRouterButton content={<Login />} lable={"Login"} />
+
+          <div>&nbsp;&nbsp;&nbsp;</div>
+
+          <AdminButtons />
         </Nav>
         <CloseProgram /> {/* Add the CloseButton component */}
       </Container>
