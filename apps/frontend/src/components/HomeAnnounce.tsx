f7d4e7587b60be142335937933d15a62cc638cd7
zmedailleu <zacharymedailleu@gmail.com>
2024-01-31 19:41:24 -0500

    scrolling announcements loop seamlessly, still need to account for when width is longer than screen

--- apps/frontend/src/components/HomeAnnounce.tsx
+++ apps/frontend/src/components/HomeAnnounce.tsx
@@ -1,17 +1,56 @@
-import React from "react";
+import React, { useReducer } from "react";
+import "./styles/HomeAnnounce.css";
 
 export const HomeAnnounce = () => {
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
+
+  const divRef = document.getElementById("marquee-content");
+  console.log(divRef === null ? 100 : divRef.getBoundingClientRect().width);
+  //const speed = (divRef===null?100:divRef.scrollWidth)/window.innerWidth;//logic
+  const pxps = 20;
+  const speed =
+    divRef === null ? 100 : divRef.getBoundingClientRect().width / pxps;
+  if (divRef === null) {
+    setTimeout(forceUpdate, 100);
+  }
+  const text =
+    "• Bob has been admitted after crashing his car into the hospital's front door • Staff Update: Wilson Wong has joined our team • Scrum Master Tommy Meet & Greet: 02/15/24 @ 3 PM • All this text is probably gonna make it overlap. I'm not a fan of this, so I wanna fix it soon.";
+  const w = divRef === null ? 100 : divRef.scrollWidth;
   return (
-    <div className={"announcements-bar"}>
-      <div className={"announcements-heading"}>
-        <b>
-          <u>Home Announcements:</u>
-        </b>
+    <div className={"announcements-bar"} id={"marquee-content"}>
+      <style>
+        {"@keyframes scroll-left {\n" +
+          `  0% {\n` +
+          `    transform: translateX(${w / 2}px);\n` +
+          "  }\n" +
+          `  100% {\n` +
+          `    transform: translateX(-${w / 2}px);\n` +
+          "  }\n" +
+          "}\n"}
+      </style>
+
+      <style>
+        {"@keyframes scroll-left2 {\n" +
+          `  0% {\n` +
+          `    transform: translateX(-${w / 2}px);\n` +
+          "  }\n" +
+          `  100% {\n` +
+          `    transform: translateX(-${3 * (w / 2)}px);\n` +
+          "  }\n" +
+          "}\n"}
+      </style>
+
+      <div
+        className={"marquee"}
+        style={{ animation: `scroll-left ${speed}s linear infinite` }}
+      >
+        {text}
       </div>
-      <div className={"marquee"}>
-        • Bob has been admitted after crashing his car into the hospital's front
-        door • Staff Update: Wilson Wong has joined our team • Scrum Master
-        Tommy Meet & Greet: 02/15/24 @ 3 PM
+      <div
+        className={"marquee"}
+        style={{ animation: `scroll-left2 ${speed}s linear infinite` }}
+      >
+        {text}
       </div>
     </div>
   );
