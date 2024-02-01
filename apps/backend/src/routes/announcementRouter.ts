b6647b75e79a67666711219498cd41425730eb20
SilasJoy <sidog2004@gmail.com>
2024-01-31 20:45:43 -0500

    Adding announcement router

---
+++ apps/backend/src/routes/announcementRouter.ts
@@ -0,0 +1,53 @@
+import express, { Router, Request, Response } from "express";
+import PrismaClient from "../bin/database-connection.ts";
+import { Prisma } from "database";
+
+const router: Router = express.Router();
+
+router.get("/", async function (req: Request, res: Response) {
+  console.log("req");
+  const announcement = await PrismaClient.announcements.findMany();
+
+  if (announcement === null) {
+    console.error("No announcements found in database!");
+    res.sendStatus(204);
+  } else {
+    res.send(announcement);
+  }
+});
+
+router.post("/", async function (req: Request, res: Response) {
+  console.log("req");
+  const requestAttempt: Prisma.AnnouncementsCreateInput = req.body;
+
+  try {
+    await PrismaClient.announcements.create({ data: requestAttempt });
+    console.log("Successfully created Announcement");
+  } catch (error) {
+    console.error("Unable to create Announcement");
+    console.log(error);
+    res.sendStatus(204);
+    return;
+  }
+
+  res.sendStatus(200);
+});
+
+router.delete("/", async function (req: Request, res: Response) {
+  console.log(req);
+
+  try {
+    await PrismaClient.announcements.delete({
+      where: { announcementID: req.body.announcementID },
+    });
+    console.log("Successfully deleted Announcement");
+  } catch (error) {
+    console.error(
+      "Announcement with ID " + req.body.announcementID + " not found!",
+    );
+    res.sendStatus(204);
+    return;
+  }
+  res.sendStatus(200);
+});
+export default router;
