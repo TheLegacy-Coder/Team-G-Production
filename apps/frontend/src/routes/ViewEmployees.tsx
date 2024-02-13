b8ab1aaa4d656b7016a561b08ad47b6b152fd199
Gabriel Shiu <gabriel@shiu.us>
2024-02-12 22:24:12 -0500

    added editing employees front end

--- apps/frontend/src/routes/ViewEmployees.tsx
+++ apps/frontend/src/routes/ViewEmployees.tsx
@@ -17,61 +17,159 @@ async function getEmployees(): Promise<EmployeeWrapper> {
 
 export const ViewEmployees = () => {
   const [rows, setRows] = useState<React.ReactElement[]>([]);
-
   const [state, setState] = useState<State>("none");
+  const [editingID, setEditingID] = useState<string>();
   const [, forceUpdate] = useReducer((x) => x + 1, 0);
 
+  const handleMouseLeave = () => {
+    if (state === "none") setEditingID(undefined);
+  };
+
   useEffect(() => {
+    const handleMouseEnter = (employeeID: string) => {
+      if (state === "none") setEditingID(employeeID);
+    };
+
+    const submit = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const form = event.currentTarget;
+      const formData = new FormData(form);
+      const data: Employee = {
+        employeeID:
+          state === "edit"
+            ? (editingID as string)
+            : (formData.get("employeeID") as string),
+        firstName: formData.get("firstName") as string,
+        lastName: formData.get("lastName") as string,
+        email: formData.get("email") as string,
+        job: formData.get("job") as string,
+        accessLevel: formData.get("accessLevel") as string,
+      };
+      console.log(data);
+      if (state === "edit") {
+        try {
+          axios
+            .patch("http://localhost:3000/api/employees", data, {})
+            .then(() => setState("none"));
+        } catch (error) {
+          console.error("Error submitting employee:", error);
+        }
+      } else if (state === "add") {
+        try {
+          axios
+            .post("http://localhost:3000/api/employees", data, {})
+            .then(() => setState("none"));
+        } catch (error) {
+          console.error("Error submitting employee:", error);
+        }
+      }
+    };
+
     getEmployees().then((list) => {
       const newRows: React.ReactElement[] = [];
       if (list !== undefined) {
         list.data.forEach((employee) => {
-          newRows.push(
-            <tr key={employee.employeeID}>
-              <td>{employee.employeeID}</td>
-              <td>{employee.firstName}</td>
-              <td>{employee.lastName}</td>
-              <td>{employee.email}</td>
-              <td>{employee.job}</td>
-              <td>{employee.accessLevel}</td>
-            </tr>,
-          );
+          const editButton =
+            employee.employeeID === editingID && state === "none" ? (
+              <button
+                style={{ position: "absolute", right: "25px" }}
+                onClick={() => setState("edit")}
+              >
+                Edit
+              </button>
+            ) : null;
+
+          if (state === "edit" && employee.employeeID === editingID) {
+            newRows.push(
+              <>
+                <form id={"empForm"} onSubmit={submit}></form>
+                <tr>
+                  <td>{employee.employeeID}</td>
+                  <td>
+                    <input
+                      name="firstName"
+                      form="empForm"
+                      required={true}
+                      defaultValue={employee.firstName}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      name="lastName"
+                      form="empForm"
+                      required={true}
+                      defaultValue={employee.lastName}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      name="email"
+                      form="empForm"
+                      required={true}
+                      defaultValue={employee.email}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      name="job"
+                      form="empForm"
+                      required={true}
+                      defaultValue={employee.job}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      name="accessLevel"
+                      form="empForm"
+                      required={true}
+                      defaultValue={employee.accessLevel}
+                    />
+                  </td>
+                </tr>
+              </>,
+            );
+          } else {
+            newRows.push(
+              <tr
+                key={employee.employeeID}
+                onMouseEnter={() => handleMouseEnter(employee.employeeID)}
+              >
+                <td>{employee.employeeID}</td>
+                <td>{employee.firstName}</td>
+                <td>{employee.lastName}</td>
+                <td>{employee.email}</td>
+                <td>{employee.job}</td>
+                <td>
+                  {employee.accessLevel}
+                  {editButton}
+                </td>
+              </tr>,
+            );
+          }
         });
       }
       if (state === "add") {
         newRows.push(
           <>
-            <form id={"newEmployee"} onSubmit={submit}></form>
+            <form id={"empForm"} onSubmit={submit}></form>
             <tr>
               <td>
-                <input
-                  name={"employeeID"}
-                  form={"newEmployee"}
-                  required={true}
-                />
+                <input name={"employeeID"} form={"empForm"} required={true} />
               </td>
               <td>
-                <input
-                  name={"firstName"}
-                  form={"newEmployee"}
-                  required={true}
-                />
+                <input name={"firstName"} form={"empForm"} required={true} />
               </td>
               <td>
-                <input name={"lastName"} form={"newEmployee"} required={true} />
+                <input name={"lastName"} form={"empForm"} required={true} />
               </td>
               <td>
-                <input name={"email"} form={"newEmployee"} required={true} />
+                <input name={"email"} form={"empForm"} required={true} />
               </td>
               <td>
-                <input name={"job"} form={"newEmployee"} required={true} />
+                <input name={"job"} form={"empForm"} required={true} />
               </td>
               <td>
-                <input
-                  name={"accessLevel"}
-                  form={"newEmployee"}
-                  required={true}
-                />
+                <input name={"accessLevel"} form={"empForm"} required={true} />
               </td>
             </tr>
           </>,
@@ -80,39 +178,14 @@ export const ViewEmployees = () => {
       setRows(newRows);
       forceUpdate();
     });
-  }, [state]);
+  }, [state, editingID]);
 
   const addEmployee = () => {
     setState("add");
   };
   const cancel = () => {
     setState("none");
-  };
-
-  const submit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("form submitted");
-    const form = event.currentTarget;
-    const formData = new FormData(form);
-    const data: Employee = {
-      employeeID: formData.get("employeeID") as string,
-      firstName: formData.get("firstName") as string,
-      lastName: formData.get("lastName") as string,
-      email: formData.get("email") as string,
-      job: formData.get("job") as string,
-      accessLevel: formData.get("accessLevel") as string,
-    };
-    console.log(data);
-    try {
-      axios
-        .post("http://localhost:3000/api/employees", data, {})
-        .then((response) => {
-          console.log(response);
-          setState("none");
-        });
-    } catch (error) {
-      console.error("Error submitting employee:", error);
-    }
+    setEditingID(undefined);
   };
 
   return (
@@ -130,12 +203,11 @@ export const ViewEmployees = () => {
             <th>accessLevel</th>
           </tr>
         </thead>
-        <tbody>{rows}</tbody>
+        <tbody onMouseLeave={handleMouseLeave}>{rows}</tbody>
       </table>
-
       {state === "add" || state === "edit" ? (
         <>
-          <input type={"submit"} form={"newEmployee"}></input>
+          <input type={"submit"} form={"empForm"}></input>
           <button onClick={cancel}>Cancel</button>
         </>
       ) : (
