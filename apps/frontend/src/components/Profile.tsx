c4ce3e02c6f5cff0731245bdd17582bc1fa1666b
Jason Zhang <zhangjason2012@gmail.com>
2024-02-12 20:45:52 -0500

    added edit profile button and stylesheet

--- apps/frontend/src/components/Profile.tsx
+++ apps/frontend/src/components/Profile.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import "./styles/Profile.css";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
@@ -15,15 +16,28 @@ const LogoutButton = () => {
   );
 };
 
-export const Profile = () => {
-  const { user, isAuthenticated } = useAuth0();
+const EditProfileButton = () => {
+  return (
+    <button
+      onClick={() => {
+        window.location; //change to actual edit profile options
+      }}
+    >
+      Edit Profile
+    </button>
+  );
+};
 
-  return isAuthenticated && user ? (
-    <div>
-      <img src={user.picture} alt={user.name} />
-      <h2>{user.name}</h2>
-      <p>{user.email}</p>
+export const Profile = () => {
+  // const { user, isAuthenticated } = useAuth0(); //user an isAuthenticated dont change to true
+
+  return (
+    /*isAuthenticated && user ? */ <div className={"profile-buttons"}>
+      {/*<img src={user.picture} alt={user.name} />*/}
+      {/*<h2>{user.name}</h2>*/}
+      {/*<p>{user.email}</p>*/}
+      <EditProfileButton />
       <LogoutButton />
-    </div>
-  ) : null;
+    </div> /*: null*/
+  );
 };
