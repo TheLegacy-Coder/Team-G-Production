0c86b3958d4242d8528542e4a6f133053e67a993
Istan <istanslamet@gmail.com>
2024-02-16 20:32:41 -0500

    added about section to context menu

---
+++ apps/frontend/src/components/About.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./styles/About.css";
+
+export const About = () => {
+  return (
+    <div className="about-page-container">
+      <div>
+        Brigham and Women’s Hospital is a world-class academic medical center
+        based in Boston, Massachusetts. The Brigham serves patients from New
+        England, across the United States and from 120 countries around the
+        world. A major teaching hospital of Harvard Medical School, Brigham and
+        Women’s Hospital has a legacy of clinical excellence that continues to
+        grow year after year.
+      </div>
+      <div>
+        The Brigham network includes 1,200 doctors throughout New England
+        working across 150 outpatient practices. An international leader in
+        virtually every area of medicine, the Brigham has led numerous medical
+        and scientific breakthroughs that have improved lives around the world.
+      </div>
+      <div>
+        U.S. News & World Report recognizes Brigham and Women’s Hospital among
+        the best hospitals in many specialty areas, including cancer, heart and
+        vascular, diabetes and endocrine disorders, ear, nose and throat,
+        gastroenterology and GI surgery, geriatric care, gynecology, neurology
+        and neurosurgery, orthopedics, pulmonology, rheumatology, and urology.
+      </div>
+    </div>
+  );
+};
