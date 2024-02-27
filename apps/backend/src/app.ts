f0ce0eeb784a83904180f24ca350b9908b6b0f35
nnewt1 <noahfnewton@gmail.com>
2024-02-26 20:11:31 -0500

    Move app.use for heatlhcheck

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -22,6 +22,14 @@ app.use(express.json()); // This processes requests as JSON
 app.use(express.urlencoded({ extended: false })); // URL parser
 app.use(cookieParser()); // Cookie parser
 
+// This is a generic path for the healthcheck. This is not publicly available anywhere
+// (e.g., Docker and the dev proxy DO NOT expose this). It exists exclusively so that can check the server
+// is alive/responsive. If this returns anything other than 200, Docker will automatically kill your backend
+// (under the assumption that something has gone horribly wrong). This must come before the authentication endpoint
+app.use("/healthcheck", function (req: Request, res: Response): void {
+  res.sendStatus(200);
+});
+
 // Setup routers. ALL ROUTERS MUST use /api as a start point, or they
 // won't be reached by the default proxy and prod setup
 app.use("/api/map", mapRouter);
@@ -47,14 +55,6 @@ app.use((err: HttpError, req: Request, res: Response): void => {
 
   // Reply with the error
   res.status(err.status || 500);
-});
-
-// This is a generic path for the healthcheck. This is not publicly available anywhere
-// (e.g., Docker and the dev proxy DO NOT expose this). It exists exclusively so that can check the server
-// is alive/responsive. If this returns anything other than 200, Docker will automatically kill your backend
-// (under the assumption that something has gone horribly wrong). This must come before the authentication endpoint
-app.use("/healthcheck", function (req: Request, res: Response): void {
-  res.sendStatus(200);
 });
 
 export default app; // Export the backend, so that www.ts can start it
